3d27cc96f62aa6146979f13304c24b6315d642cb
Scarce Koi <111240985+scarcekoi@users.noreply.github.com>
2024-12-29 12:41:38 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,9 @@ import typographyPlugin from '@tailwindcss/typography';
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-      colors: {
-        primary: 'var(--aw-color-primary)',
-      },
+    colors: {
+      primary: 'var(--aw-color-primary)',
+    },
     extend: {
       colors: {
         primary: 'var(--aw-color-primary)',
@@ -15,6 +15,18 @@ export default {
         accent: 'var(--aw-color-accent)',
         default: 'var(--aw-color-text-default)',
         muted: 'var(--aw-color-text-muted)',
+        // Add default gray scale here
+        gray: {
+          100: '#f7fafc',
+          200: '#edf2f7',
+          300: '#e2e8f0',
+          400: '#cbd5e0',  // this is the default gray-400
+          500: '#a0aec0',
+          600: '#718096',
+          700: '#4a5568',
+          800: '#2d3748',
+          900: '#1a202c',
+        },
       },
       fontFamily: {
         sans: ['var(--aw-font-sans, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
@@ -39,7 +51,7 @@ export default {
     plugin(({ addVariant }) => {
       addVariant('intersect', '&:not([no-intersect])');
     }),
-    require("@catppuccin/tailwindcss"),
+    require('@catppuccin/tailwindcss'),
   ],
   darkMode: 'class',
 };
