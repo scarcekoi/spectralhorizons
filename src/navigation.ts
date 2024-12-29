d1d784ec1a90822069189131c3ebf35e4cd8be5a
Scarce Koi <111240985+scarcekoi@users.noreply.github.com>
2024-12-28 20:11:51 -0500

    Update navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -4,6 +4,7 @@ export const headerData = {
   links: [
     {
       text: 'Homes',
+      href: getPermalink('/homes'),
       links: [
         {
           text: 'SaaS',
@@ -165,19 +166,9 @@ export const footerData = {
       ],
     },
   ],
-  secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
-  ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'Youtube', icon: 'tabler:brand-youtube', href: 'https://twitter.com/SpectralHorizon' },
+    { ariaLabel: 'Twitter', icon: 'tabler:brand-twitter', href: 'https://youtube.com/@spectralhorizons' },
+    { ariaLabel: 'Bandcamp', icon: 'tabler:brand-bandcamp', href: 'https://spectralhorizons.bandcamp.com' },
   ],
-  footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
-  `,
 };
